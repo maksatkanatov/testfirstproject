4cf24578f300f314be3684ca3dad1155e6bcf7d2
Max <maksatkanat@gmail.com>
2022-11-19 13:11:49 +0600

    git init project

---
+++ src/main/java/kz/narxoz/testfirstproject/TestfirstprojectApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.testfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfirstprojectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestfirstprojectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kz/narxoz/testfirstproject/TestfirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.testfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestfirstprojectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
